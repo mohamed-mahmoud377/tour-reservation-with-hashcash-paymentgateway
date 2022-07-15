c54a0322253e1b1fe23a3512e9851e3bc3b21124
mohamed mahmoud <mohamedMahmoud3776@gmail.com>
2022-07-16 00:11:47 +0200

    init

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,5 +1,5 @@
 const Tour = require("./../models/tourModel");
-
+const axios = require("axios")
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const Booking= require("./../models/bookingModel")
 const User= require("./../models/userModel")
@@ -13,27 +13,37 @@ exports.getCheckoutSession = catchAsync(async (req,res,next)=>{
 
     //1 get the currently booked tour
     const  tour= await Tour.findById(req.params.tourId);
+    try{
+        const response  = axios.post('http://www.hashcash.digital/api/checkout/',{
+
+        })
+
+    }catch (e) {
+
+    }
+
+
 
     //create checkout session
-    const session = await stripe.checkout.sessions.create({   //////////////////////////////////////////////////
-        payment_method_types: ['card'],
-        // success_url: `${req.protocol}://${req.get('host')}/?tour=${tour.id}&user=${req.user.id}&price=${tour.price}`, // in case if the payment success
-        success_url:`${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
-        cancel_url:`${req.protocol}://${req.get('host')}/tour/${tour.slug}`, // in case the user canceling the image
-        customer_email: req.user.email,
-        client_reference_id: req.params.tourId,
-        line_items: [{
-            name: `${tour.name} Tour`,
-            description: tour.summary,
-            images:[`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
-            amount: tour.price * 100, //because it will be in cent
-            currency: 'usd',
-            quantity: 1
-
-        }]
-
-
-    })
+    // const session = await stripe.checkout.sessions.create({   //////////////////////////////////////////////////
+    //     payment_method_types: ['card'],
+    //     // success_url: `${req.protocol}://${req.get('host')}/?tour=${tour.id}&user=${req.user.id}&price=${tour.price}`, // in case if the payment success
+    //     success_url:`${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
+    //     cancel_url:`${req.protocol}://${req.get('host')}/tour/${tour.slug}`, // in case the user canceling the image
+    //     customer_email: req.user.email,
+    //     client_reference_id: req.params.tourId,
+    //     line_items: [{
+    //         name: `${tour.name} Tour`,
+    //         description: tour.summary,
+    //         images:[`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
+    //         amount: tour.price * 100, //because it will be in cent
+    //         currency: 'usd',
+    //         quantity: 1
+    //
+    //     }]
+    //
+    //
+    // })
     //create session as response
     res.status(200).json({
         status: 'success',
